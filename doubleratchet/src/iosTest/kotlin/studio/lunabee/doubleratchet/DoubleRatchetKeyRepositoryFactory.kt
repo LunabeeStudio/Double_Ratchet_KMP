156d2a4d484c567daac44f5b6b9f685f500de78b
rlatapy-luna <remi.latapy@lunabee.com>
2023-07-19 22:20:22 +0200

    Provide ConstantDoubleRatchetKeyRepository for ios

--- doubleratchet/src/iosTest/kotlin/studio/lunabee/doubleratchet/DoubleRatchetKeyRepositoryFactory.kt
+++ doubleratchet/src/iosTest/kotlin/studio/lunabee/doubleratchet/DoubleRatchetKeyRepositoryFactory.kt
@@ -20,7 +20,5 @@ import studio.lunabee.doubleratchet.crypto.DoubleRatchetKeyRepository
 import kotlin.random.Random
 
 actual object DoubleRatchetKeyRepositoryFactory {
-    actual fun getRepository(random: Random): DoubleRatchetKeyRepository {
-        TODO("Not yet implemented")
-    }
+    actual fun getRepository(random: Random): DoubleRatchetKeyRepository = ConstantDoubleRatchetKeyRepository
 }
