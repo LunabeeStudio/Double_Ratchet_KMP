e23a2a2281eb8914fc805637737c2e058662e52d
rlatapy-luna <remi.latapy@lunabee.com>
2023-07-20 00:04:07 +0200

    bump version

--- doubleratchet/build.gradle.kts
+++ doubleratchet/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 
 group = "studio.lunabee.doubleratchet"
 description = "Kotlin multiplatform implementation of double ratchet algorithm"
-version = "0.1.0"
+version = "0.1.1"
 
 kotlin {
     jvm {
