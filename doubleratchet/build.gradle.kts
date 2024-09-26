c8c50929ffa6a48516a66bb3476095b472c60157
Tom Kubasik <83946830+tkubasik-luna@users.noreply.github.com>
2024-09-26 15:09:45 +0200

    bump version (#12)
    ## 📜 Description
    0.1.5
    ## 💡 Motivation and Context
    
    ## 💚 How did you test it?
    launch app os
    ## 📝 Checklist
    
    * [ ] 📖 I reviewed the submitted code
    * [ ] 🛀 I launched `./gradlew detekt`
    * [ ] 🏭 I implemented Unit Tests

--- doubleratchet/build.gradle.kts
+++ doubleratchet/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 
 group = "studio.lunabee.doubleratchet"
 description = "Kotlin multiplatform implementation of double ratchet algorithm"
-version = "0.1.4-uuid-error-SNAPSHOT"
+version = "0.1.5"
 
 kotlin {
     jvm {
